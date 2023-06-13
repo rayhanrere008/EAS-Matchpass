fd7599e7fc0291d6fd39301071e917a7cd9db923
Rayhan Qalby Ramadhan <125190070+rayhanrere008@users.noreply.github.com>
2023-06-13 15:46:27 +0700

    Update awal.php

--- Admin/awal.php
+++ Admin/awal.php
@@ -355,7 +355,7 @@
                     <div class="modal-body">Silahkan klik "Keluar" jika Anda yakin.</div>
                     <div class="modal-footer">
                         <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
-                        <a class="btn btn-primary" href="login.html">Keluar</a>
+                        <a class="btn btn-primary" href="index.html">Keluar</a>
                     </div>
                 </div>
             </div>
